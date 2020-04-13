2b8386160ee9df7eedeac97e4d2c87ab9b967911
Iurii-Antoniuk <iurii.antoniuk@gmail.com>
2020-04-13 16:52:58 +0200

    first commit

---
+++ WPFApp/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media.Imaging;
+using DemoLibrary;
+
+namespace WPFApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private int maxNumber = 0;
+        private int currentNumber = 0;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            ApiHelper.InitializeClient();
+        }
+
+        private async Task LoadImage(int imageNumber = 0)
+        {
+            var comic = await ComicProcessor.LoadComic(imageNumber);
+
+            if (imageNumber == 0)
+            {
+                maxNumber = comic.Num;
+            }
+            
+            currentNumber = comic.Num;
+            var imageSource = new Uri(comic.Img, UriKind.Absolute);
+            comicImage.Source = new BitmapImage(imageSource);
+        }
+    }
+}
